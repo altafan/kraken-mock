84dd1f4fbe83feb051b4bf1868298caaa410d3f3
Louis <louis@vulpem.com>
2024-03-29 12:52:19 +0100

    dockerize the application

--- main.go
+++ main.go
@@ -209,6 +209,7 @@ func getBalance(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("kraken mock server started")
 	http.HandleFunc("/0/private/AddOrder", newOrder)
 	http.HandleFunc("/0/private/QueryOrders", queryOrders)
 	http.HandleFunc("/0/private/Balance", getBalance)
